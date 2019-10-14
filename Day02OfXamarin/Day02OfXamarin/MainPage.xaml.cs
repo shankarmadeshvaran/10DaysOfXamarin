f9afbfbdf78a43bed735647d1e2c0a1826895c59
shankarmadeshvaran <shankarmadeshvaran@gmail.com>
2019-10-14 14:51:57 +0530

    Day 01 Project completed

--- Day02OfXamarin/Day02OfXamarin/MainPage.xaml.cs
+++ Day02OfXamarin/Day02OfXamarin/MainPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace Day1OfXamarin
+namespace Day02OfXamarin
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
@@ -16,17 +16,6 @@ namespace Day1OfXamarin
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(username.Text)) {
-                greeting_Label.Text = null;
-                DisplayAlert("Error","Your username can't be empty","OK");
-            } else
-            {
-                greeting_Label.Text = $"Hello {username.Text}!!! Welcome to Xamarin Development";
-            }
         }
     }
 }
